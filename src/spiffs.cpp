6a5d66f8c23be3105ae38a50070070443f06695a
Pedro Gomes <pedronsg@gmail.com>
2018-08-24 19:05:55 +0100

    added spiffs class

---
+++ src/spiffs.cpp
@@ -0,0 +1,27 @@
+#include "spiffs.h"
+
+
+void Spiffs::begin()
+{
+  SPIFFS.begin();
+
+  Serial.println("\nSearching files...");
+   Dir existDir = SPIFFS.openDir("/");
+   if (existDir.next())
+   {
+     Serial.println("Spiffs already formatted");
+   }else
+   {
+     Serial.println("Please wait 30 secs for SPIFFS to be formatted");
+     SPIFFS.format();
+     Serial.println("Spiffs formatted");
+     File f = SPIFFS.open("/formatComplete.txt", "w");
+   }
+  Dir dir = SPIFFS.openDir("/");
+while (dir.next()) {
+    Serial.print(dir.fileName());
+    File f = dir.openFile("r");
+    Serial.print(" ");
+    Serial.println(f.size());
+}
+}
---
+++ src/spiffs.h
@@ -0,0 +1,15 @@
+#ifndef SPIFFS_H
+#define SPIFFS_H
+
+#include "FS.h"
+
+class Spiffs
+{
+  private:
+
+  public:
+    void begin();
+};
+
+
+#endif
