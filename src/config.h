a300ed636562f574a6c506ea1a27924f9d9a5d07
Pedro Gomes <pedronsg@gmail.com>
2018-08-24 19:02:03 +0100

    Added more macros for configurations

--- src/config.h
+++ src/config.h
@@ -1,11 +1,16 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#define STOP_SWITCH D5
 #define PUMP_PIN D6
-#define UNPROTECTED_START_DELAY 10000 //milliseconds avoiding pump protection to discard high amp values
-#define MAX_RUNNING_TIME 1 //maximum value of minutes for flood protection
-#define NO_WATER_TIME 30 //seconds before enter in nowater mode
+#define UNPROTECTED_START_DELAY 5000 //milliseconds avoiding pump protection to discard high amp values
+#define MAX_RUNNING_TIME 30 //maximum value of minutes for flood protection
+#define NO_WATER_TIME 20 //seconds before enter in nowater mode
 #define DEBOUNCE 10 //milliseconds to debounce values
+
+#define DISPLAYADDRESS 0x3c
+#define SDAPIN D2
+#define SCLPIN D1
 
   struct ConfigData{
   //    char status[10]="NOTSET";
